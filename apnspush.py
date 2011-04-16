0b84d81d6f7f2f73782d83d839f96b73ac63db3f
Cameron Villers <cameron@ville.rs>
2011-04-16 13:39:51 -0400

    - added install instructions - changed config file to have named api keys and added -u argument to specify which clients to push to

--- apnspush.py
+++ apnspush.py
@@ -14,7 +14,7 @@ EPILOG = "API keys must be put in the configuration file (default %s)." % DEFAUL
 parser = argparse.ArgumentParser(description = "Client for App Notifications (4push) for iOS", epilog = EPILOG)
 
 parser.add_argument("--config", help="config file location", default=DEFAULT_CONFIG)
-parser.add_argument("--debug", type=bool, default=False)
+parser.add_argument("--debug", type=bool, default=False, metavar='')
 parser.add_argument("-t", "--title", help="message title", type=str)
 parser.add_argument("-s", "--subtitle", help="message subtitle", type=str)
 parser.add_argument("-p", "--long-message-preview", help="long message preview", type=str)
@@ -22,14 +22,15 @@ parser.add_argument("-i", "--icon-url", help="icon URL", type=str)
 parser.add_argument("-v", "--message-level", help="message severity", choices=range(-2, 3), type=int)
 parser.add_argument("-b", "--button-text", help="text on the action button (only displays when mobile device is not locked)", type=str)
 parser.add_argument("-c", "--command", help="command to run when the action button is invoked (in iOS URI format)", type=str)
+parser.add_argument("-u", "--users", help="list of users defined in the configuration file to send the message to (default all)", type=str, default="all", dest="clients")
 
 soundGroup = parser.add_mutually_exclusive_group()
-soundGroup.add_argument("-q", "--silent-message", help="do not play a sound for the recipient", type=bool, default=False)
+soundGroup.add_argument("-q", "--silent-message", help="do not play a sound for the recipient", type=bool, default=False, metavar='')
 soundGroup.add_argument("-n", "--sound", help="sound to play on the mobile device", choices=range(1, 8), type=int, default=1)
 
 longGroup = parser.add_mutually_exclusive_group()
 longGroup.add_argument("-l", "--long-message", help="long message displayed in application", type=str)
-longGroup.add_argument("-r", "--read-long-message", help="read the long message from standard input", type=bool)
+longGroup.add_argument("-r", "--read-long-message", help="read the long message from standard input", type=bool, metavar='')
 
 parser.add_argument("message", help="message text displayed in notfication", type=str, action="store")
 
@@ -37,7 +38,7 @@ args = parser.parse_args()
 
 ###############################################################################
 
-if not os.path.isfile(args.config)
+if not os.path.isfile(args.config):
 	print("Could not load config file %s" % args.config)
 	sys.exit(1)
 
@@ -56,5 +57,15 @@ else:
 
 ###############################################################################
 
-for key in config["keys"]:
-	notifications.send(key, args.message, args.title, args.subtitle, longMessage, args.long_message_preview, args.icon_url, args.message_level, args.silent_message, args.button_text, args.command, args.sound, args.debug)
+clients = []
+
+if args.clients == "all":
+	clients = config["keys"].keys()
+else:
+	clients = args.clients.split(',')
+
+for key in clients:
+	if not key in config["keys"]:
+		print("Unknown client %s" % key)
+		sys.exit(2)
+	notifications.send(config["keys"][key], args.message, args.title, args.subtitle, longMessage, args.long_message_preview, args.icon_url, args.message_level, args.silent_message, args.button_text, args.command, args.sound, args.debug)
